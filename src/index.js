6c162e71e4f942bdd4165a69d74893951eeb585c
unknown <lenhack1@gmail.com>
2022-02-12 23:24:19 +0200

    task is complite

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  
+  const objectOfBrackets = {};
+  const openBrackets = [];
+  
+  for(let i = 0; i < bracketsConfig.length; i++){
+    objectOfBrackets[bracketsConfig[i][1]] = bracketsConfig[i][0];
+    openBrackets.push(bracketsConfig[i][0]);
+  } 
+
+
+  let stack = [];
+  for(let i = 0; i < str.length; i++){
+    let cur = str[i];
+   
+    if(stack.length === 0){
+      if(openBrackets.includes(cur)){
+        stack.push(cur);
+      } else {
+        return false;
+      }
+    } else {
+      if(objectOfBrackets[cur] === stack[stack.length - 1]){
+        stack.pop();
+      } else if(openBrackets.includes(cur)){
+        stack.push(cur);
+      }
+    }    
+  }
+   return stack.length === 0;
 }
